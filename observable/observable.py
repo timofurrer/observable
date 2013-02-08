89017d9065462f27fec0ad69fc8b608df7e3d8c5
Timo Furrer <tuxtimo@gmail.com>
2013-02-08 16:42:41 +0100

    added basic implementation of Observable

---
+++ observable/observable.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+
+class Observable:
+    """event system for python"""
+    class NoHandlerFound(Exception):
+        """Raised if no handler for event was found"""
+        def __init__(self, event):
+            Exception.__init__(self, "No handler for event '%s' found" % event)
+
+    def __init__(self):
+        self._events = {}
+
+    def on(self, event, func=None):
+        """register a handler to a specified event"""
+        def _on_wrapper(func):
+            if event not in self._events:
+                self._events[event] = []
+            self._events[event].append(func)
+            return func
+
+        if func:
+            return _on_wrapper(func)
+        else:
+            return _on_wrapper
+
+    def once(self, event, func=None):
+        """register a handler to a specified event, but remove it when it is triggered"""
+        def _once_wrapper(func):
+            def _wrapper(*args, **kwargs):
+                func(*args, **kwargs)
+                self.remove_handler(event, _wrapper)
+            return _wrapper
+
+        if func:
+            return self.on(event, _once_wrapper(func))
+        else:
+            return lambda func: self.on(event, _once_wrapper(func))
+
+    def remove_handler(self, event, func):
+        """remove a handler from a specified event"""
+        if event not in self._events:
+            raise Observable.NoHandlerFound(event)
+        self._events[event].remove(func)
+
+    def clear_handlers(self, event):
+        """remove all handlers from a specified event"""
+        if event not in self._events:
+            raise Observable.NoHandlerFound(event)
+        self._events[event] = []
+
+    def clear_events(self):
+        """remove all events and thier handlers"""
+        self._events = {}
+
+    def trigger(self, event, *args, **kwargs):
+        """trigger all functions from an event"""
+        if event not in self._events:
+            raise Observable.NoHandlerFound(event)
+
+        handled = False
+        for func in self._events[event]:
+            func(*args, **kwargs)
+            handled = True
+        return handled
