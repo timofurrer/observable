c18c40078e73fd896340db77c48d1b4aae0527e0
Timo Furrer <tuxtimo@gmail.com>
2013-02-08 16:12:47 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name="observable",
+    description="event system for python",
+    version="0.00.01",
+    author="Timo Furrer",
+    author_email="tuxtimo@gmail.com",
+    url="https://github.com/timofurrer/pyobservable",
+    packages=["observable"]
+)
