acb0b02d3c8b5af5003e68ba790e657200ffdf76
Robert Schindler <r.schindler@efficiosoft.com>
2018-06-02 17:16:13 +0200

    Added type annotations for Python 3.

--- observable/core.py
+++ observable/core.py
@@ -4,38 +4,42 @@
     Event system for python
 """
 
+import typing as T
+
 from collections import defaultdict
 
 
 class HandlerNotFound(Exception):
     """Raised if a handler wasn't found"""
 
-    def __init__(self, event, handler):
+    def __init__(self, event: str, handler: T.Callable) -> None:
+        super().__init__()
         self.event = event
         self.handler = handler
 
-    def __str__(self):
-        return "Handler {} wasn't found for event {}".format(self.handler,
-                                                             self.event)
+    def __str__(self) -> str:
+        return "Handler {} wasn't found for event {}" \
+               .format(self.handler, self.event)
 
 
 class EventNotFound(Exception):
     """Raised if an event wasn't found"""
 
-    def __init__(self, event):
+    def __init__(self, event: str) -> None:
+        super().__init__()
         self.event = event
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "Event {} wasn't found".format(self.event)
 
 
 class Observable(object):
     """Event system for python"""
 
-    def __init__(self):
-        self._events = defaultdict(list)
+    def __init__(self) -> None:
+        self._events = defaultdict(list)  # type: T.DefaultDict[str, T.List[T.Callable]]
 
-    def get_all_handlers(self):
+    def get_all_handlers(self) -> T.Dict[str, T.List[T.Callable]]:
         """Returns a dict with event names as keys and lists of
         registered handlers as values."""
 
@@ -44,21 +48,21 @@ class Observable(object):
             events[event] = list(handlers)
         return events
 
-    def get_handlers(self, event):
+    def get_handlers(self, event: str) -> T.List[T.Callable]:
         """Returns a list of handlers registered for the given event."""
 
         return list(self._events.get(event, []))
 
-    def is_registered(self, event, handler):
+    def is_registered(self, event: str, handler: T.Callable) -> bool:
         """Returns whether the given handler is registered for the
         given event."""
 
         return handler in self._events.get(event, [])
 
-    def on(self, event, *handlers):  # pylint: disable=invalid-name
+    def on(self, event: str, *handlers: T.Callable) -> T.Callable:  # pylint: disable=invalid-name
         """Register a handler to a specified event"""
 
-        def _on_wrapper(*handlers):
+        def _on_wrapper(*handlers: T.Callable) -> T.Callable:
             """wrapper for on decorator"""
             self._events[event].extend(handlers)
             return handlers[0]
@@ -67,33 +71,14 @@ class Observable(object):
             return _on_wrapper(*handlers)
         return _on_wrapper
 
-    def once(self, event, *handlers):
-        """Register a handler to a specified event, but remove it when it is triggered"""
-
-        def _once_wrapper(*handlers):
-            """Wrapper for 'once' decorator"""
-
-            def _wrapper(*args, **kw):
-                """Call wrapper"""
-                for handler in handlers:
-                    handler(*args, **kw)
-
-                self.off(event, _wrapper)
-
-            return _wrapper
-
-        if handlers:
-            return self.on(event, _once_wrapper(*handlers))
-        return lambda x: self.on(event, _once_wrapper(x))
-
-    def off(self, event=None, *handlers):
+    def off(self, event: str = None, *handlers: T.Callable) -> bool:
         """Unregister an event or handler from an event"""
 
         if not event:
             self._events.clear()
             return True
 
-        if not event in self._events:
+        if event not in self._events:
             raise EventNotFound(event)
 
         if not handlers:
@@ -107,15 +92,33 @@ class Observable(object):
                 self._events[event].remove(callback)
         return True
 
+    def once(self, event: str, *handlers: T.Callable) -> T.Callable:
+        """Register a handler to a specified event, but remove it when it is triggered"""
 
-    def trigger(self, event, *args, **kw):
+        def _once_wrapper(*handlers: T.Callable) -> T.Callable:
+            """Wrapper for 'once' decorator"""
+
+            def _wrapper(*args: T.Any, **kw: T.Any) -> None:
+                """Call wrapper"""
+                for handler in handlers:
+                    handler(*args, **kw)
+
+                self.off(event, _wrapper)
+
+            return _wrapper
+
+        if handlers:
+            return self.on(event, _once_wrapper(*handlers))
+        return lambda x: self.on(event, _once_wrapper(x))
+
+    def trigger(self, event: str, *args: T.Any, **kw: T.Any) -> bool:
         """Trigger all functions which are subscribed to an event"""
 
-        functions = self._events.get(event)
-        if not functions:
+        callbacks = self._events.get(event)
+        if not callbacks:
             return False
 
-        for event in functions:
-            event(*args, **kw)
+        for callback in callbacks:
+            callback(*args, **kw)
 
         return True
