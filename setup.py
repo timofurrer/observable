2692a0f6f5e78825c74a7aa97d3583f64b789838
Timo Furrer <tuxtimo@gmail.com>
2018-06-09 10:34:23 +0200

    Do not test for Python 3.4

--- setup.py
+++ setup.py
@@ -86,6 +86,7 @@ setup(
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation",
